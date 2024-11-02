91c18b1b69b813077cc2692705f760230d44fab9
RafaelZingra <rafaelzingra@hotmail.com>
2024-11-02 17:22:55 -0300

    adds tons of code just to send radio button value

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -2,14 +2,14 @@ import * as Dialog from '@radix-ui/react-dialog'
 import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles'
 import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
 import * as z from 'zod'
-import { useForm } from 'react-hook-form'
+import { Controller, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 
 const newTransactionFormSchema = z.object( {
     description: z.string(),
     price: z.number(),
     category: z.string(),
-    // type: z.enum(['income','outcome'])
+    type: z.enum(['income','outcome'])
 })
 
 type NewTransactionFormSchema = z.infer<typeof newTransactionFormSchema>
@@ -17,6 +17,7 @@ type NewTransactionFormSchema = z.infer<typeof newTransactionFormSchema>
 export function NewTransactionModal() {
 
     const {
+        control,
         register,
         handleSubmit,
         formState: {isSubmitting}
@@ -57,17 +58,28 @@ export function NewTransactionModal() {
                         required
                         {...register('category')}
                     />
+                    <Controller 
+                        control={control}
+                        name='type'
+                        render={({field}) => {
+                            return (
+                                <TransactionType 
+                                    onValueChange={field.onChange}
+                                    value={field.value}
+                                >
+                                    <TransactionTypeButton variant='income' value='income'>
+                                        <ArrowCircleUp size={24}/>
+                                        Entrada
+                                    </TransactionTypeButton>
+                                    <TransactionTypeButton variant='outcome' value='outcome'>
+                                        <ArrowCircleDown size={24}/>
+                                        Saída
+                                    </TransactionTypeButton>
+                                </TransactionType>
+                            )
+                        }}
+                    />
                     
-                    <TransactionType>
-                        <TransactionTypeButton variant='income' value='income'>
-                            <ArrowCircleUp size={24}/>
-                            Entrada
-                        </TransactionTypeButton>
-                        <TransactionTypeButton variant='outcome' value='outcome'>
-                            <ArrowCircleDown size={24}/>
-                            Saída
-                        </TransactionTypeButton>
-                    </TransactionType>
                     <button type="submit" disabled={isSubmitting}>
                         Cadastrar
                     </button>
