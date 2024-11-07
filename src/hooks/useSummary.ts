44c317399eef543a3816f15ae9bf1679ba90334e
RafaelZingra <rafaelzingra@hotmail.com>
2024-11-06 21:32:12 -0300

    performance stuff

--- src/hooks/useSummary.ts
+++ src/hooks/useSummary.ts
@@ -1,27 +1,32 @@
-import { useContext } from "react"
+import { useMemo } from "react"
 import { TransactionsContext } from "../contexts/TransactionContext"
+import { useContextSelector } from "use-context-selector"
 
 export function useSummary () {
     
-    const { transactions } = useContext(TransactionsContext)
+    const transactions = useContextSelector(TransactionsContext, (context) => {
+        return context.transactions
+    })
 
-    const summary = transactions.reduce(
-        (acc, transaciton) => {
-            if (transaciton.type === 'income') {
-                acc.income += transaciton.price
-                acc.total += transaciton.price
-            } else {
-                acc.outcome += transaciton.price
-                acc.total -= transaciton.price
+    const summary = useMemo(() => {
+        return transactions.reduce(
+            (acc, transaciton) => {
+                if (transaciton.type === 'income') {
+                    acc.income += transaciton.price
+                    acc.total += transaciton.price
+                } else {
+                    acc.outcome += transaciton.price
+                    acc.total -= transaciton.price
+                }
+                return acc
+            }, 
+            {
+                income:0,
+                outcome:0,
+                total:0
             }
-            return acc
-        }, 
-        {
-            income:0,
-            outcome:0,
-            total:0
-        }
-    )
+        )
+    }, [transactions])
 
     return summary
 }
--- src/pages/transactions/components/SearchForm/indext.tsx
+++ src/pages/transactions/components/SearchForm/indext.tsx
@@ -3,8 +3,9 @@ import { SearchFormContainer } from "./styles";
 import { MagnifyingGlass } from 'phosphor-react'
 import * as z from 'zod'
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useContext } from "react";
 import { TransactionsContext } from "../../../../contexts/TransactionContext";
+import { useContextSelector } from "use-context-selector";
+import { memo } from "react";
 
 const searchFormSchema = z.object({
     query: z.string(),
@@ -12,9 +13,11 @@ const searchFormSchema = z.object({
 
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
-export function SearchForm () {
+function SearchFormComponent () {
 
-    const{ fetchTransactions } = useContext(TransactionsContext)
+    const fetchTransactions = useContextSelector(TransactionsContext, (context) => {
+        return context.fetchTransactions
+    })
 
     const { 
         register, 
@@ -42,3 +45,5 @@ export function SearchForm () {
         </SearchFormContainer>                
     )
 }
+
+export const SearchForm = memo(SearchFormComponent)
